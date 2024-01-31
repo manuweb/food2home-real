60d061de6840073df3a874a708c7a434651bd910
Manuel  Sánchez <m.sanchez@elmaestroweb.es>
2024-01-31 12:27:12 +0100

    Add files via upload

--- admin/includes/leeclientes.php
+++ admin/includes/leeclientes.php
@@ -24,31 +24,39 @@ $checking=false;
 $tamPagina=12;
 
 
-$sql="SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero FROM `pedidos_clientes` WHERE ".$filtro." GROUP BY pedidos_clientes.email  UNION SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero FROM usuarios_app WHERE ".$filtro." ORDER BY ".$orden.";";
+$sql="SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero FROM pedidos_clientes WHERE ".$filtro." GROUP BY pedidos_clientes.email  UNION SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero FROM usuarios_app WHERE ".$filtro." ORDER BY ".$orden.";";
 
 
-$sql="(SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero, '0' AS idCliente, '0' as totalcompras  FROM `pedidos_clientes` WHERE ".$filtro." GROUP BY pedidos_clientes.email  )
+$sql="(SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero, '0' AS idCliente, '0' as totalcompras  FROM pedidos_clientes WHERE ".$filtro." GROUP BY pedidos_clientes.email)
 
 UNION
 
 (
- 
- SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, 0 as totalcompras 
-FROM usuarios_app WHERE ".$filtro." GROUP BY usuarios_app.username
+SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, 0 as totalcompras 
+FROM usuarios_app WHERE ".$filtro." AND usuarios_app.id
 
-INTERSECT 
-    
-SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, sum(pedidos.total) as totalcompras 
-FROM usuarios_app  left join  pedidos on pedidos.cliente=usuarios_app.id WHERE ".$filtro." AND pedidos.estadoPago=1 GROUP BY usuarios_app.username
+NOT IN 
 
+(SELECT  usuarios_app.id 
+FROM usuarios_app  left join  pedidos on pedidos.cliente=usuarios_app.id WHERE ".$filtro." AND pedidos.estadoPago=1)
 )
 
 UNION 
-(
-    SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, sum(pedidos.total) as totalcompras 
+
+(SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, CAST(SUM(pedidos.total) AS INTEGER) as totalcompras 
 FROM usuarios_app  left join  pedidos on pedidos.cliente=usuarios_app.id WHERE ".$filtro." AND pedidos.estadoPago=1 GROUP BY usuarios_app.username
 )
+
 ORDER BY ".$orden." DESC;";
+
+
+
+
+
+
+//file = fopen("zz_detalle_compras.txt", "w");
+//fwrite($file, "sql: ". $sql . PHP_EOL);
+//fclose($file);
 
 $sumamonedero=0;
 $sumamPedidos=0;
@@ -64,37 +72,38 @@ while ($clientesM = $result->fetch_object()) {
 }
 $limitInf=($pagina-1)*$tamPagina;
 
-$sql="SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero, '0' AS idCliente FROM `pedidos_clientes` WHERE ".$filtro." GROUP BY pedidos_clientes.email  UNION SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente FROM usuarios_app WHERE ".$filtro." ORDER BY apellidos, nombre DESC LIMIT ".$limitInf.",".$tamPagina.";";
+$sql="SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero, '0' AS idCliente FROM pedidos_clientes WHERE ".$filtro." GROUP BY pedidos_clientes.email  UNION SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente FROM usuarios_app WHERE ".$filtro." ORDER BY apellidos, nombre DESC LIMIT ".$limitInf.",".$tamPagina.";";
 
-$sql="(SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero, '0' AS idCliente, '0' as totalcompras  FROM `pedidos_clientes` WHERE ".$filtro." GROUP BY pedidos_clientes.email  )
+
+$sql="(SELECT 'N' AS tipo, pedidos_clientes.tratamiento AS tratamiento, pedidos_clientes.apellidos AS apellidos , pedidos_clientes.nombre AS nombre, pedidos_clientes.email AS email, pedidos_clientes.publicidad as publicidad, pedidos_clientes.telefono as telefono, '0' AS monedero, '0' AS idCliente, 0 as totalcompras  FROM pedidos_clientes WHERE ".$filtro." GROUP BY pedidos_clientes.email)
 
 UNION
 
 (
- 
- SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, 0 as totalcompras 
-FROM usuarios_app WHERE ".$filtro." GROUP BY usuarios_app.username
+SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, 0 as totalcompras 
+FROM usuarios_app WHERE ".$filtro." AND usuarios_app.id
 
-INTERSECT 
-    
-SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, sum(pedidos.total) as totalcompras 
-FROM usuarios_app  left join  pedidos on pedidos.cliente=usuarios_app.id WHERE ".$filtro." AND pedidos.estadoPago=1 GROUP BY usuarios_app.username
+NOT IN 
 
+(SELECT  usuarios_app.id 
+FROM usuarios_app  left join  pedidos on pedidos.cliente=usuarios_app.id WHERE ".$filtro." AND pedidos.estadoPago=1)
 )
 
 UNION 
-(
-    SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, sum(pedidos.total) as totalcompras 
+
+(SELECT 'S' AS tipo, usuarios_app.tratamiento AS tratamiento, usuarios_app.apellidos as apellidos  , usuarios_app.nombre as nombre, usuarios_app.username as email, usuarios_app.publicidad as publicidad, usuarios_app.telefono as telefono,usuarios_app.monedero as monedero, usuarios_app.id AS idCliente, CAST(SUM(pedidos.total) AS INTEGER) as totalcompras 
 FROM usuarios_app  left join  pedidos on pedidos.cliente=usuarios_app.id WHERE ".$filtro." AND pedidos.estadoPago=1 GROUP BY usuarios_app.username
 )
+
 ORDER BY ".$orden." DESC LIMIT ".$limitInf.",".$tamPagina.";";
 
 
-/*
-$file = fopen("zz_detalle_compras.txt", "w");
-fwrite($file, "sql: ". $sql . PHP_EOL);
-fclose($file);
-*/
+
+
+//$file = fopen("zz_detalle_compras.txt", "w");
+//fwrite($file, "sql: ". $sql . PHP_EOL);
+//fclose($file);
+
 $database = DataBase::getInstance();
 $database->setQuery($sql);
 $result = $database->execute();
@@ -113,6 +122,7 @@ if ($result) {
         $publicidad[$n]=$clientes->publicidad;
         $monedero[$n]=$clientes->monedero;
         $Compras[$n]=$clientes->totalcompras;
+        
         $n++;
     }
     
