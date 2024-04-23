747b2844df05e33a7d0f7ff4ba3364dc8c6353c9
Manuel  Sánchez <m.sanchez@elmaestroweb.es>
2024-04-23 12:54:14 +0200

    Add files via upload

---
+++ admin/servidor/test.php
@@ -0,0 +1,93 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+header("Access-Control-Allow-Origin: *");
+include "../../webapp/conexion.php";
+include "../../webapp/MySQL/DataBase.class.php";
+
+
+//select pedidos.id, pedidos.numero, pedidos.numeroRevo, pedidos.estadoPago, pedidos.fecha FROM pedidos WHERE fecha like '2023-12-16%' ORDER BY pedidos.fecha DESC; 
+
+$sql="SELECT pedidos.id, pedidos.numero, pedidos.numeroRevo, pedidos.estadoPago, pedidos.fecha , pedidos.anulado, pedidos.total FROM pedidos  WHERE fecha >= '".date("Y")."-".date("m")."-".date("d")." 00:00:01' ORDER BY `pedidos`.`id` DESC;";
+
+
+//echo "sql:<br>";
+//echo $sql;
+//echo "<hr>";
+$database = DataBase::getInstance();
+$database->setQuery($sql);
+$result = $database->execute();
+$fechaFinal = date('d-m-Y H:i:s');
+$fechaFinalSegundos = strtotime($fechaFinal);
+$aquitar = [];
+echo "<style> td {border: solid 1px;padding:2px;}</style>";
+    echo "<table>";
+while ($pedidos = $result->fetch_object()) {
+    $fecha=$pedidos->fecha;
+    $id=$pedidos->id;
+    $numero=$pedidos->numero;
+    $numeroRevo=$pedidos->numeroRevo;
+    $estadoPago=$pedidos->estadoPago;
+    $estadoAnula=$pedidos->anulado;
+    $importe=$pedidos->total;
+    $estado='';
+    $color='';
+
+    
+    if ($estadoPago==-1){
+        $estado='ANULADO';
+        $color='style="color:red;"';
+    }
+    if ($estadoPago==0){
+        $estado='PENDIENTE';
+        $color='style="color:black;"';
+    }
+    if ($estadoPago==1){
+        $estado='PAGADO';
+        $color='style="color:black;"';
+    }
+    if ($estadoAnula==1){
+        $estado='ANULADO';
+        $color='style="color:gray;"';
+    }
+    if ($estadoAnula==2){
+        $estado='ES ANULACION';
+        $color='style="color:darkorange;"';
+    }
+    $fechaInicial = date($fecha);
+    
+    $NuevaFecha = strtotime ( '+1 hour' , strtotime ($fechaInicial) ) ;
+    //$fechaInicial = date ( 'Y-m-d H:i:s' , $NuevaFecha); 
+
+    // Las convertimos a segundos
+    $fechaInicialSegundos = strtotime($fechaInicial);
+    
+    
+    // Hacemos las operaciones para calcular los dias entre las dos fechas y mostramos el resultado
+    $segundos=($fechaFinalSegundos - $fechaInicialSegundos);
+    $minutos = $segundos / 60;
+    echo "<tr>";
+    //if ($minutos>15){
+        $aquitar[]=$id;
+        echo "<td>".$id."</td><td><b>".$numero."</b></td><td> <b>".$numeroRevo."</b></td><td><span ".$color."><b>".$estado."</b></span></td><td style='text-align: right;'>".$importe."</td> <td><b>". round($minutos)  . "</b> minutos.</td>" ;
+    echo "</tr>";
+    //}
+
+}
+
+echo "</table>";
+/*
+for($x=0;$x<count($aquitar);$x++){
+    $sql='UPDATE pedidos SET estadoPago=-1 WHERE id='.$aquitar[$x];
+    $database->setQuery($sql);
+    $result2 = $database->execute();
+    if ($result2) {
+        echo "Pedido=".$aquitar[$x]. "actualizado<br>";
+    }
+    
+}
+*/
+
+$database->freeResults();  
+
+?>
---
+++ admin/servidor/updatePedidos.php
@@ -0,0 +1,99 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+include "../../webapp/conexion.php";
+include "../../webapp/MySQL/DataBase.class.php";
+
+
+
+
+$sql="SELECT pedidos.id, pedidos.numero, pedidos.numeroRevo, pedidos.cliente, pedidos.estadoPago, pedidos.fecha, pedidos.importe_fidelizacion, pedidos.monedero FROM pedidos  WHERE fecha >= '".date("Y")."-".date("m")."-".date("d")." 00:00:01' ORDER BY `pedidos`.`id` DESC;";
+
+//echo "sql:<br>";
+//echo $sql;
+//echo "<hr>";
+$database = DataBase::getInstance();
+$database->setQuery($sql);
+$result = $database->execute();
+$fechaFinal = date('d-m-Y H:i:s');
+$fechaFinalSegundos = strtotime($fechaFinal);
+
+
+$aquitar = [];
+
+while ($pedidos = $result->fetch_object()) {
+    $fecha=$pedidos->fecha;
+    $id=$pedidos->id;
+    $numero=$pedidos->numero;
+    $numeroRevo=$pedidos->numeroRevo;
+    $estadoPago=$pedidos->estadoPago;
+    $importe_fidelizacion=$pedidos->importe_fidelizacion;
+    $monedero=$pedidos->monedero;
+    $cliente=$pedidos->cliente;
+    $estado='';
+    $color='';
+    if ($estadoPago==-1){
+        $estado='ANULADO';
+        $color='style="color:red;"';
+    }
+    if ($estadoPago==0){
+        $estado='PENDIENTE';
+        $color='style="color:black;"';
+    }
+    if ($estadoPago==1){
+        $estado='PAGADO';
+        $color='style="color:black;"';
+    }
+    
+    $fechaInicial = date($fecha);
+    
+    $NuevaFecha = strtotime ( '+1 hour' , strtotime ($fechaInicial) ) ;
+    //$fechaInicial = date ( 'Y-m-d H:i:s' , $NuevaFecha); 
+
+    // Las convertimos a segundos
+    $fechaInicialSegundos = strtotime($fechaInicial);
+    
+    
+    // Hacemos las operaciones para calcular los dias entre las dos fechas y mostramos el resultado
+    $segundos=($fechaFinalSegundos - $fechaInicialSegundos);
+    $minutos = $segundos / 60;
+    
+
+        echo "(".$id.") nº: <b>".$numero."</b> idRevo: <b>".$numeroRevo."</b> Estado: <span ".$color."><b>".$estado."</b></span> Tiempo: <b>". round($minutos)  . "</b> minutos.<br>" ;
+    if ($estadoPago==0){
+        if (round($minutos)>15){
+            if ($numeroRevo=='0'){
+                $aquitar[]=$id;
+                $fidelizacion[]=$importe_fidelizacion;
+                $losClientes[]=$cliente;
+                $losMonederos[]=$monedero;
+            }
+        }
+    }
+}
+        
+echo "<hr>";
+for($x=0;$x<count($aquitar);$x++){
+
+    $sql='UPDATE pedidos SET estadoPago=-1 WHERE id='.$aquitar[$x].' AND numeroRevo="0"';
+    echo $sql;
+    echo "<hr>";
+    $database->setQuery($sql);
+    $result2 = $database->execute();
+    if ($result2) {
+        /*
+        if ($fidelizacion[$x]>0){
+            $sql2="UPDATE usuarios_app SET monedero=monedero+".($losMonederos[$x]-$fidelizacion[$x])." WHERE id=".$losClientes[$x].";";
+            
+            $database->setQuery($sql2);
+            $result3 = $database->execute();
+        }
+        */
+        echo "Pedido=".$aquitar[$x]. "actualizado<br>";
+    }
+
+}
+
+$database->freeResults();  
+
+?>
