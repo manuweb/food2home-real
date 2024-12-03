d0ea00a2421333c6c611363af24db336ee566caf
Manuel  Sánchez <m.sanchez@elmaestroweb.es>
2024-12-03 16:39:42 +0100

    Update productos.js

--- admin/js/productos.js
+++ admin/js/productos.js
@@ -947,6 +947,9 @@ $('#boton-add-grupo').attr('onclick','editaProducto(0,'+grupo+',\''+nombregrupo+
                     if (esMenu[x]=='2'){
                         textoesMenu='<i class="icon f7-icons size-14">layers_alt_fil</i> '+nombre[x];
                     }
+                     if (esMenu[x]=='5'){
+                        textoesMenu='<i class="icon f7-icons size-14">gigtcard</i> '+nombre[x];
+                    }
 
                     if(imagen[x]==''){
                         img2='../webapp/img/productos/no-imagen.jpg';
